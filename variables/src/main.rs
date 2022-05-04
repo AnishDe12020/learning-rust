7ab7142b0a3bedb8394107452c8b34962a3cc37b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-04 14:19:53 +0530

    variables: add shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,19 @@ fn main() {
 
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("Three hours in seconds: {}", THREE_HOURS_IN_SECONDS);
+
+    let y = 76;
+    let y = y + 1;
+
+    {
+        let y = y + 2;
+        println!("The value of y in the inner scope is: {}", y);
+    }
+
+    println!("The value of y is: {}", y);
+
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    println!("All the letters: {}", letters);
+    let letters = letters.len();
+    println!("The length of letters is: {}", letters);
 }
