ace92d355b3bb3bc03c7d11d74de493cdb396536
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-04 14:16:50 +0530

    add constant in variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds: {}", THREE_HOURS_IN_SECONDS);
 }
