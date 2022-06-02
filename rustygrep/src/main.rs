9ab0e7b1d141e0551b96542b6a52644e98869f08
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-02 16:37:06 +0530

    rustygrep: print errors to std err

--- rustygrep/src/main.rs
+++ rustygrep/src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {} in {}", config.query, config.filename);
 
     if let Err(e) = rustygrep::run(config) {
-        println!("Error: {}", e);
+        eprintln!("Error: {}", e);
         process::exit(1);
     }
 }
