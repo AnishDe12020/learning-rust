62388ecc62aaefb4f3f9f44853f70f1d7bde07e7
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-02 16:20:33 +0530

    rustygrep: refactor run and config logic to a library crate

--- rustygrep/src/main.rs
+++ rustygrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use rustygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,33 +13,8 @@ fn main() {
 
     println!("Searching for {} in {}", config.query, config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rustygrep::run(config) {
         println!("Error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("Contents:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
