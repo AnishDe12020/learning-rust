12a30a5fa7ae7ad503fd95ed54c4b8296e5ec186
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-05 14:40:55 +0530

    wasm-gol: implement the basic game of life by creating a universe

--- wasm-game-of-life/www/index.js
+++ wasm-game-of-life/www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("ser");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  console.log(universe.render());
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
