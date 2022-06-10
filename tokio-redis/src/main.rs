9dc4820c3448a552370a45b7cd4c5b4bcb078c0c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-10 18:06:59 +0530

    tokio-redis: add a mutex for shared state between tasks

--- tokio-redis/src/main.rs
+++ tokio-redis/src/main.rs
@@ -1,25 +1,35 @@
+use bytes::Bytes;
+use mini_redis::Command::{self, Get, Set};
 use mini_redis::{Connection, Frame};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
+        let db = db.clone();
+
+println!("Accepted");
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis.
     let mut connection = Connection::new(socket);
@@ -27,18 +37,20 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
             }
             cmd => panic!("unimplemented: {:?}", cmd),
         };
-connection.write_frame(&response).await.unwrap();
+        connection.write_frame(&response).await.unwrap();
     }
 }
