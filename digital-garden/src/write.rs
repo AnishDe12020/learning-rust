a04bb6caef82c9d75f0558d07d1d04e1f48bbc97
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-07-09 14:35:28 +0530

    feat(digital-garden): make the user confirm filename else ask for a new one

--- digital-garden/src/write.rs
+++ digital-garden/src/write.rs
@@ -2,6 +2,7 @@ use color_eyre::eyre::{Result, WrapErr};
 use edit::{edit_file, Builder};
 use std::{
     io::{Read, Seek, SeekFrom, Write},
+    os::linux::raw,
     path::PathBuf,
 };
 
@@ -30,7 +31,42 @@ pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
             .map(|maybe_line| maybe_line.trim_start_matches("# ").to_string())
     });
 
-    dbg!(contents, document_title);
+    let filename = match document_title {
+        Some(raw_title) => confirm_filename(&raw_title),
+        None => ask_for_filename(),
+    };
+
+    dbg!(contents, filename);
 
     todo!()
+}
+
+fn ask_for_filename() -> Result<String> {
+    rprompt::prompt_reply_stderr(
+        "\
+Enter filename
+> ",
+    )
+    .wrap_err("Failed to get filename")
+    .map(|title| slug::slugify(title))
+}
+
+fn confirm_filename(raw_title: &str) -> Result<String> {
+    loop {
+        let result = rprompt::prompt_reply_stderr(&format!(
+            "\
+current title: `{}`
+Do you want a different title? (y/N): ",
+            raw_title,
+        ))
+        .wrap_err("Failed to get input for y/n question")?;
+
+        match result.as_str() {
+            "y" | "Y" => break ask_for_filename(),
+            "n" | "N" | "" => {
+                break Ok(slug::slugify(raw_title));
+            }
+            _ => {}
+        };
+    }
 }
