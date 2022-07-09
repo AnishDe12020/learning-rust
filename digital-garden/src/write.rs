736101a4cf6035f841381b5c3b63490b770d0d5d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-07-09 14:17:32 +0530

    feat(digital-garden): added the ability to write content

--- digital-garden/src/write.rs
+++ digital-garden/src/write.rs
@@ -1,7 +1,28 @@
-use color_eyre::Result;
-use std::path::PathBuf;
+use color_eyre::eyre::{Result, WrapErr};
+use edit::{edit_file, Builder};
+use std::{
+    io::{Read, Write},
+    path::PathBuf,
+};
+
+const TEMPLATE: &[u8; 2] = b"# ";
 
 pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
-    dbg!(garden_path, title);
+    let (mut file, filepath) = Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&garden_path)
+        .wrap_err("Failed to create wip file")?
+        .keep()
+        .wrap_err("Failed to keep temp file")?;
+    file.write_all(TEMPLATE)?;
+
+    edit_file(filepath)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    dbg!(contents);
+
     todo!()
 }
